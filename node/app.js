43b0caa87e8ccc663a6b3aae76b714395701d493
Thomas Weber <weber@openinnovator.net>
2016-07-13 17:47:52 +0200

    Qlearning pres slides

--- node/app.js
+++ node/app.js
@@ -2,13 +2,19 @@ var serialport = require('serialport'),
     SerialPort = serialport.SerialPort,
     sport = process.argv[2],
     webport = process.argv[3] || 3000,
-
+    qtable = {},
     fs = require('fs'),
     net = require('net'),
-    app = require('http').createServer((req,res) => {
+    app = require('http').createServer(function (req,res) {
         // Serves static files from public directory
+        if (req.url == '/qtable') {
+            res.setHeader('content-type', 'application/json');
+            res.write(JSON.stringify(qtable));
+            res.end();
+            return;
+        }
         if (req.url == '/') req.url = '/index.html';
-        fs.readFile(__dirname + '/public' + req.url, (err, data) => {
+        fs.readFile(__dirname + '/public' + req.url, function (err, data) {
             if (err) {
                 res.writeHead(404);
                 res.write('404: File not found.');
@@ -31,8 +37,15 @@ var serialport = require('serialport'),
     }),
     io = require('socket.io')(app);
 
-io.on('connection', (iosocket) => {
-    console.log("Socket IO connection established.")
+io.on('connection', function (iosocket) {
+    console.log("Socket IO connection established.");
+});
+
+io.on('qlearn', function (data) {
+    data = JSON.parse(data);
+    for (var s in data) {
+        qtable[s] = data[s];
+    }
 });
 
 const ARDUINO_DATA_BREATH = 0x01;
@@ -42,13 +55,13 @@ var arduino = new SerialPort(sport, {
     baudRate: 9600,
     options: false,
     parser: serialport.parsers.readline("\n")
-}).on('open', () => {
+}).on('open', function () {
     console.log('Serial port is open');
-}).on('close', () => {
+}).on('close', function () {
     console.log('Serial port is closed');
-}).on('error', (e) => {
+}).on('error', function (e) {
     console.log('Error: \n\t' + e);
-}).on('data', (data) => {
+}).on('data', function (data) {
     console.log("Data from Arduino: " + data);
     var jsonData = JSON.parse(data);
     switch (jsonData.type) {
@@ -61,7 +74,8 @@ var arduino = new SerialPort(sport, {
     }
 });
 
-app.listen(webport, () => {
+
+app.listen(webport, function () {
     var address = app.address();
     console.log("Web-Server at %j", address);
 });
