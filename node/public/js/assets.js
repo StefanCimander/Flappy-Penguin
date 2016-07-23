8adb870a102181bdf8aa9e89774eedcb9086e3cd
Thomas Weber <weber@openinnovator.net>
2016-07-23 23:20:03 +0200

    Credit where credit is due

--- node/public/js/assets.js
+++ node/public/js/assets.js
@@ -3,4 +3,4 @@ const MALE_PENGUIN_SPRITE = "assets/square/pingu_male.png",
       FEMALE_PENGUIN_SPRITE = "assets/square/pingu_female.png",
       FEMALE_PENGUIN_DIVER_SPRITE = "assets/square/pingu_female_diver.png",
       BUBBLE = "assets/square/bubble.png",
-      MUSIC_FILE = "assets/bensound-slowmotion.mp3";
+      MUSIC_FILE = "assets/bensound-slowmotion.mp3"; // http://www.bensound.com/royalty-free-music/track/slow-motion
